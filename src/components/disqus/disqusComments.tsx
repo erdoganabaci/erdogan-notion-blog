d030e9dcef02dd62fa7e655182fef40ff5339571
erdoganabaci <erdoganabaci97@gmail.com>
2021-08-19 03:17:56 +0300

    add new disqus url

--- src/components/disqus/disqusComments.tsx
+++ src/components/disqus/disqusComments.tsx
@@ -1,6 +1,15 @@
 import { DiscussionEmbed } from "disqus-react"
+import { useEffect, useState } from "react";
 
 const DisqusComments = ({ id, title, postSlug }) => {
+    const [disqusConfig, setDisqusConfig] = useState({});
+
+    useEffect(() => {
+        setDisqusConfig({ url: "https://erdogan-blog.vercel.app/blog/" + postSlug, identifier: id, title });
+    }, [])
+
+    console.log('disqus config ', disqusConfig)
+
     const disqusShortname = "erdoganabaci"
     // const disqusConfig = {
     //     url: `http://localhost:3000/blog/${postSlug}`,
@@ -11,13 +20,7 @@ const DisqusComments = ({ id, title, postSlug }) => {
         <div>
             <DiscussionEmbed
                 shortname={disqusShortname}
-                config={
-                    {
-                        url: `https://erdogan-blog.vercel.app/blog/${postSlug}`,
-                        identifier: id,
-                        title
-                    }
-                }
+                config={disqusConfig}
             />
         </div>
     )
